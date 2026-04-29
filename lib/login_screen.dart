9559093f8e6f0a61ed0fc5f6559163578f7a0f4e
DarkloverGitHub <yashyadva2026@gmail.com>
2026-04-29 14:44:18 +0545

    text changed

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -117,7 +117,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     );
                   },
                   child: const Text(
-                    "Login",
+                    "signin",
                     style: TextStyle(
                         fontSize: 16, fontWeight: FontWeight.bold),
                   ),
